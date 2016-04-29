efdfc917d0d654c92c9b262b116f67398af618d2
bashi <ishibashi.kenichi@gmail.com>
2016-04-29 10:44:37 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# dotenv
+
+Set environment variables from the `.env` file in the current directory and run
+a given command.
+
+This is a work-in-progress utility. It only works under some basic usage.
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+const (
+	env_filename = ".env"
+)
+
+func parseLine(r *bufio.Reader) (string, string, error) {
+	line, err := r.ReadString('\n')
+	if err != nil {
+		return "", "", err
+	}
+	line = strings.TrimSpace(line)
+	if line[0] == '#' {
+		return "", "", nil
+	}
+	fields := strings.Split(line, "=")
+	if len(fields) != 2 {
+		return "", "", fmt.Errorf("Failed to parse line: %s", line)
+	}
+	return fields[0], fields[1], nil
+}
+
+func setEnvFromReader(r io.Reader) error {
+	br := bufio.NewReader(r)
+	for {
+		key, value, err := parseLine(br)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		// TODO: Don't expand when a value is enclosed by single quotes.
+		value = os.ExpandEnv(value)
+		os.Setenv(key, value)
+	}
+	return nil
+}
+
+func setEnvFromFile(path string) error {
+	file, err := os.Open(path)
+	if err != nil {
+		// Nop when the given path doesn't exist.
+		if os.IsNotExist(err) {
+			return nil
+		}
+		return err
+	}
+	defer file.Close()
+	return setEnvFromReader(file)
+}
+
+func execute(name string, args []string) error {
+	cmd := exec.Command(name, args...)
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return err
+	}
+	go func() {
+		defer stdin.Close()
+		_, err := io.Copy(stdin, os.Stdin)
+		if err != nil {
+			panic(err)
+		}
+	}()
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func run(name string, args []string) error {
+	err := setEnvFromFile(env_filename)
+	if err != nil {
+		return err
+	}
+	return execute(name, args)
+}
+
+func main() {
+	if err := run(os.Args[1], os.Args[2:]); err != nil {
+		panic(err)
+	}
+}
